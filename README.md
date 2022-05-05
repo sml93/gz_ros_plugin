4109aaaac8c35a2bee1c73721fcc3838623de287
sml93 <shawnmlee93@gmail.com>
2022-05-06 01:06:54 +0800

    Second commit. Check README to follow up on edits and TODO.

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ If you are installing this package onto a new PC for the first time, make sure t
       <plugin name='model_push_plugin' filename="/home/your-pc-name/catkin_ws/devel/lib/libmodel_push_plugin.so"/>
       after the last plugin.
 -----> roslaunch px4 posix_sitl.launch and you should see a force applied to the iris model
+
+
+Date of update: 05 May 2022
+Edited: jetForce topics to take in float mag and int angle to allow for negative angles, read /mavros/local_position/pose to track uav movement.
+TODO: Check on jetForce angle rotation accuracy. Seems like negative angles pushes the uav back and vice versa. To create a counter to automatically send 0 jetForce mag after a certain time.
--- src/jetForce.cc
+++ src/jetForce.cc
@@ -12,6 +12,7 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <math.h>
+#include "geometry_msgs/PoseStamped.h"
 
 namespace gazebo
 {
@@ -31,6 +32,7 @@ namespace gazebo
       // Create a topic name
       std::string jetForce_angle_topicName = "/jetForce_angle";
       std::string jetForce_mag_topicName = "/jetForce_mag";
+      std::string pose_topicName = "/mavros/local_position/pose";
 
       if (!ros::isInitialized())
       {
@@ -57,7 +59,7 @@ namespace gazebo
       
       // Magnitude
       ros::SubscribeOptions so2 = 
-        ros::SubscribeOptions::create<std_msgs::Int16>(
+        ros::SubscribeOptions::create<std_msgs::Float32>(
           jetForce_mag_topicName, 1,
           boost::bind(&jetForce::OnRosMsg_Mag, this, _1),
           ros::VoidPtr(), &this->rosQueue2);
@@ -66,6 +68,18 @@ namespace gazebo
       // Spin up the queue helper thread.
       this->rosQueueThread2 = 
         std::thread(std::bind(&jetForce::QueueThread2, this));
+
+      // Magnitude
+      ros::SubscribeOptions so3 = 
+        ros::SubscribeOptions::create<geometry_msgs::PoseStamped>(
+          pose_topicName, 1,
+          boost::bind(&jetForce::OnRosMsg_Pose, this, _1),
+          ros::VoidPtr(), &this->rosQueue3);
+      this->rosSub3 = this->rosNode->subscribe(so3);
+
+      // Spin up the queue helper thread.
+      this->rosQueueThread3 = 
+        std::thread(std::bind(&jetForce::QueueThread3, this));
      
 
     }
@@ -75,12 +89,22 @@ namespace gazebo
       double jetAng = this->x_axis_angle;
       double jetforce_x = jetMag * sin(jetAng);
       double jetforce_z = jetMag * cos(jetAng);
-      ROS_WARN("jetAng >> %f", jetAng);
-      ROS_WARN("jetMag >> %f", jetMag);
 
-      // Apply a small linear velocity to the model.
-      this->model->SetLinearVel(ignition::math::Vector3d(jetforce_x, 0, jetforce_z));
-      // this->model->SetAngularVel(ignition::math::Vector3d(0, 0, 0));
+      // ROS_WARN("jetAng >> %f", jetAng);
+      // ROS_WARN("jetMag >> %f", jetMag);
+
+      ROS_WARN("x_current >> %f", this->x_current);
+      ROS_WARN("x_current >> %f", this->z_current);
+
+      if (jetMag != 0)
+      {
+        // ROS_WARN("jetforce_x >> %f", jetforce_x);
+        // ROS_WARN("jetforce_z >> %f", jetforce_z);
+        // Apply a small linear/angular velocity to the model.
+        this->model->SetLinearVel(ignition::math::Vector3d(jetforce_x, 0, jetforce_z));
+        // this->model->SetAngularVel(ignition::math::Vector3d(jetforce_x, 0, 0));
+      }
+      // TODO: Write subscriber to the pose topic to get the current x and z and add into the SetLinearVel
     }
 
     
@@ -104,9 +128,19 @@ namespace gazebo
     }
 
 
-    public: void OnRosMsg_Mag(const std_msgs::Int16ConstPtr &_msg)
+    public: void OnRosMsg_Mag(const std_msgs::Float32ConstPtr &_msg)
     {
       this->SetMag(_msg->data);
+    }
+
+    public: void OnRosMsg_Pose(const geometry_msgs::PoseStamped::ConstPtr &_msg)
+    {
+      // ROS_INFO_STREAM("Received Pose: " << _msg);
+      this->x_current = _msg->pose.position.x;
+      this->y_current = _msg->pose.position.y;
+      this->z_current = _msg->pose.position.z;
+      ROS_WARN("x_current >> %f", x_current);
+      ROS_WARN("z_current >> %f", z_current);
     }
 
 
@@ -117,7 +151,7 @@ namespace gazebo
       static const double timeout = 0.01;
       while(this->rosNode->ok())
       {
-        this->rosQueue2.callAvailable(ros::WallDuration(timeout));
+        this->rosQueue.callAvailable(ros::WallDuration(timeout));
       }
     }
 
@@ -128,6 +162,16 @@ namespace gazebo
       while (this->rosNode->ok())
       {
         this->rosQueue2.callAvailable(ros::WallDuration(timeout));
+      }
+    }
+
+    ///\brief ROS Helper function3 that processes messages
+    private: void QueueThread3()
+    {
+      static const double timeout = 0.01;
+      while (this->rosNode->ok())
+      {
+        this->rosQueue3.callAvailable(ros::WallDuration(timeout));
       }
     }
 
@@ -142,6 +186,10 @@ namespace gazebo
     double x_axis_angle = 0.0;
     double x_axis_mag = 0.0;
 
+    double x_current;
+    double y_current;
+    double z_current;
+
     private: std::unique_ptr<ros::NodeHandle> rosNode;
     
     private: ros::Subscriber rosSub;
@@ -151,6 +199,10 @@ namespace gazebo
     private: ros::Subscriber rosSub2;
     private: ros::CallbackQueue rosQueue2;
     private: std::thread rosQueueThread2;
+
+    private: ros::Subscriber rosSub3;
+    private: ros::CallbackQueue rosQueue3;
+    private: std::thread rosQueueThread3;
 
   };
 
